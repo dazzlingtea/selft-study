324b304504ed62a7c0ccdb4f359d27a032e52748
dazzlingtea <kitsnr08@gmail.com>
2024-03-12 22:10:07 +0900

    백준String 5622번

---
+++ src/String5622.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class String5622 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] arr = sc.next().toCharArray();
+		int[] dial = new int[]{2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9};
+		int result = 0;
+		for(char i : arr) {
+			byte ascii = (byte)i;
+			int num = dial[ascii - 65];
+			result += num + 1;		
+		}
+		System.out.println(result);
+	}
+
+}
