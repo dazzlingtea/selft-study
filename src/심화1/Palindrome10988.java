e9c9d0f7273ea823dbba35297eb9122a7bd5b993
dazzlingtea <kitsnr08@gmail.com>
2024-05-07 19:51:48 +0900

    심화1 팰린드롬 10988번

---
+++ src/심화1/Palindrome10988.java
@@ -0,0 +1,21 @@
+package 심화1;
+
+import java.util.Scanner;
+
+public class Palindrome10988 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+        int half = (int) Math.floor(word.length() / 2);
+
+        for (int i = 0; i < half; i++) {
+            if(word.charAt(i) != word.charAt(word.length() -i -1)) {
+                System.out.println(0);
+                return;
+            }
+        }
+        System.out.println(1);
+
+    }
+}
