4909dc4d79df93426119dba851037416dccbee1e
dazzlingtea <kitsnr08@gmail.com>
2024-05-20 17:48:56 +0900

    약수배수 2501

--- src/factorprime/Factor2501.java
+++ src/factorprime/Factor2501.java
@@ -9,5 +9,13 @@ public class Factor2501 {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
+        int[] arr = new int[n];
+
+        for (int i = 0; i < Math.floor(Math.sqrt(n)); i++) {
+            if(n % i == 0){
+                arr[i] = i;
+            }
+        }
+
     }
 }
