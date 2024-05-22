a6a1bfe8d47ce2aa79bc75436079fd5f286ba473
dazzlingtea <kitsnr08@gmail.com>
2024-05-22 19:56:33 +0900

    기하1 실선둘레 15894

---
+++ src/geo1/Perimeter15894.java
@@ -0,0 +1,13 @@
+package geo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Perimeter15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(4 * n);
+    }
+}
