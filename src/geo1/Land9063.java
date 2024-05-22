87853db79699d4a696c33998537e2eb0a56d72f4
dazzlingtea <kitsnr08@gmail.com>
2024-05-22 20:19:17 +0900

    기하1 대지 9063

---
+++ src/geo1/Land9063.java
@@ -0,0 +1,35 @@
+package geo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Land9063 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        if(n == 1) {
+            System.out.println(0);
+            return;
+        }
+        int minX = 10001;
+        int minY = 10001;
+        int maxX = -10001;
+        int maxY = -10001;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+        System.out.println((maxX - minX) * (maxY - minY));
+
+    }
+}
