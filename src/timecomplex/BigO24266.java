3e815a2250a4fe32049d79e6b26dbe05e3062474
dazzlingtea <kitsnr08@gmail.com>
2024-05-24 17:14:07 +0900

    시간복잡도 5

---
+++ src/timecomplex/BigO24266.java
@@ -0,0 +1,13 @@
+package timecomplex;
+
+import java.util.Scanner;
+
+public class BigO24266 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
