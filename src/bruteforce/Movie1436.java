a71463e57e5c51299673c98c591f106eea89948f
dazzlingtea <kitsnr08@gmail.com>
2024-06-01 16:44:24 +0900

    브루트포스 1436

---
+++ src/bruteforce/Movie1436.java
@@ -0,0 +1,22 @@
+package bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Movie1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 666;
+        int count = 1;
+        while(count != n) {
+            result++;
+            if(String.valueOf(result).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(result);
+    }
+}
+
