80bb3b8fe49d7fe873e5b96b32233fe80b92b079
dazzlingtea <kitsnr08@gmail.com>
2024-06-04 18:54:27 +0900

    정렬 25305

--- src/sort/Cutline25305.java
+++ src/sort/Cutline25305.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Cutline25305 {
     public static void main(String[] args) throws IOException {
