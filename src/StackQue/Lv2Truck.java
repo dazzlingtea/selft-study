25078339de1d341f99a80253cf6e29212ee4da77
dazzlingtea <kitsnr08@gmail.com>
2024-07-28 18:12:26 +0900

    스택큐 트럭 다른 풀이

--- src/StackQue/Lv2Truck.java
+++ src/StackQue/Lv2Truck.java
@@ -11,7 +11,7 @@ import java.util.*;
  */
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<int[]> afterBridge = new LinkedList<>();
+        Queue<int[]> onBridge = new LinkedList<>();
         Queue<Integer> trucks = new LinkedList<>();
         for(int target : truck_weights){
             trucks.add(target);
@@ -19,21 +19,21 @@ class Solution {
 
         int currWeight = 0;
         int time = 0;
-        while(!trucks.isEmpty() || !afterBridge.isEmpty()){
-            if(!afterBridge.isEmpty()
-                    && afterBridge.peek()[0] <= time
+        while(!trucks.isEmpty() || !onBridge.isEmpty()){
+            if(!onBridge.isEmpty()
+                    && onBridge.peek()[0] <= time
             ){
-                int [] temp = afterBridge.poll();
+                int [] temp = onBridge.poll();
                 currWeight -= temp[1];
             }
 
             if(!trucks.isEmpty()
-                    && afterBridge.size() + 1 <= bridge_length
+                    && onBridge.size() + 1 <= bridge_length
                     && currWeight + trucks.peek() <= weight
             ){
                 int temp = trucks.poll();
                 currWeight += temp;
-                afterBridge.add(new int [] {time + bridge_length,temp});
+                onBridge.add(new int [] {time + bridge_length,temp});
             }
             time++;
         }
@@ -43,11 +43,68 @@ class Solution {
 
 public class Lv2Truck {
     public static void main(String[] args) {
-        int bridge_length = 0;
-        int weight = 0;
-        int[] truck_weights = {};
+        int bridge_length = 100;
+        int weight = 100;
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
 
         int answer = new Solution().solution(bridge_length, weight, truck_weights);
         System.out.println(answer);
     }
 }
+//import java.util.*;
+//
+//class Solution {
+//    class Truck {
+//        int weight;
+//        int move;
+//
+//        public Truck(int weight) {
+//            this.weight = weight;
+//            this.move = 1;
+//        }
+//
+//        public void moving() {
+//            move++;
+//        }
+//    }
+//
+//    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+//        Queue<Truck> waitQ = new LinkedList<>();
+//        Queue<Truck> moveQ = new LinkedList<>();
+//
+//        for (int t : truckWeights) {
+//            waitQ.offer(new Truck(t));
+//        }
+//
+//        int answer = 0;
+//        int curWeight = 0;
+//
+//        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+//            answer++;
+//
+//            if (moveQ.isEmpty()) {
+//                Truck t = waitQ.poll();
+//                curWeight += t.weight;
+//                moveQ.offer(t);
+//                continue;
+//            }
+//
+//            for (Truck t : moveQ) {
+//                t.moving();
+//            }
+//
+//            if (moveQ.peek().move > bridgeLength) {
+//                Truck t = moveQ.poll();
+//                curWeight -= t.weight;
+//            }
+//
+//            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+//                Truck t = waitQ.poll();
+//                curWeight += t.weight;
+//                moveQ.offer(t);
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
