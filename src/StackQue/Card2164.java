2515af4901d06dc596672d042c17940c64b7b3b9
dazzlingtea <kitsnr08@gmail.com>
2024-12-03 22:18:32 +0900

    백준 2164 카드2

---
+++ src/StackQue/Card2164.java
@@ -0,0 +1,28 @@
+package StackQue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Card2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+        System.out.println("카드: " + q);
+        while (q.size() > 1) {
+//            if(q.size() < 2) {
+//                System.out.println(q.poll());
+//                break;
+//            }
+            q.poll();
+//            System.out.println(q);
+            q.add(q.poll());
+//            System.out.println(q);
+        }
+        System.out.println(q.poll());
+    }
+}
